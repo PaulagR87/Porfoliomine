918156a20659cad6a44d64d9113b83929a7147b2
Paula Rodriguez <paula.g.rodriguez@gmail.com>
2021-11-02 18:40:21 -0300

    s

--- js/multimedia.js
+++ js/multimedia.js
@@ -28,38 +28,41 @@ let track = document.createElement('audio');
 let All_song = [
    {
      name: "first song",
-     path: "music/song1.mp3",
+     path: "music/1.mp3",
      img: "img/img1.jpg",
      singer: "1"
    },
    {
      name: "second song",
-     path: "music/song2.mp3",
-     img: "img/img2.jpg",
+     path: "2.mp3",
+     img: "img2.jpg",
      singer: "2"
    },
    {
      name: "third song",
-     path: "music/song3.mp3",
-     img: "img/img3.jpg",
+     path: "3.mp3",
+     img: "img3.jpg",
      singer: "3"
    },
    {
      name: "fourth song",
-     path: "music/song4.mp3",
-     img: "img/img4.jpg",
+     path: "4.mp3",
+     img: "img4.jpg",
      singer: "4"
    },
    {
      name: "fifth song",
-     path: "music/song5.mp3",
-     img: "img/img5.jpg",
+     path: "5.mp3",
+     img: "img5.jpg",
      singer: "5"
    }
 ];
 
 
+// All functions
 
+
+// function load the track
 function load_track(index_no){
 	clearInterval(timer);
 	reset_slider();
@@ -166,7 +169,7 @@ function autoplay_switch(){
        auto_play.style.background = "rgba(255,255,255,0.2)";
 	}else{
        autoplay = 1;
-       auto_play.style.background = "#FF8A65";
+       auto_play.style.background = "#148F77";
 	}
 }
 
